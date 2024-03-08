2edf41186c541f1423499ce7fdd281c07a86d202
ossan <ivan.pesenti.dev@gmail.com>
2024-03-08 10:48:45 +0100

    global vars managed

--- main.go
+++ main.go
@@ -1,10 +1,15 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
+
+var PersonGreeted string = ""
 
 func main() {
 	fmt.Println("Type your name:")
 	var name string
 	fmt.Scan(&name)
 	fmt.Printf("Hi, %s!\n", name)
+	PersonGreeted = name
 }
